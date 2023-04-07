70223ba43df0a4ce493bbf0f549a91c7ba812b41
Cancerwangmonkey <130135430+Cancerwangmonkey@users.noreply.github.com>
2023-04-07 16:12:05 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 /*
- * 將要隨機派發的網址放入下方（不需排序）
+ *
  'https://www.surveycake.com/s/lZO7r','https://www.surveycake.com/s/dprvB','https://www.surveycake.com/s/nZ49m','https://www.surveycake.com/s/VgnXP','https://www.surveycake.com/s/Z61zd','https://www.surveycake.com/s/MP2bD','https://www.surveycake.com/s/DPxLb','https://www.surveycake.com/s/X6BA0','https://www.surveycake.com/s/2ogk2'
  */
 
