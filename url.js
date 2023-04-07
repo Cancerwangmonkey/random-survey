6c8c1b9964ad45e8d2fec16340f9e9fe25208b80
Cancerwangmonkey <130135430+Cancerwangmonkey@users.noreply.github.com>
2023-04-07 16:08:22 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,9 +1,9 @@
 /*
- * 將要隨機派發的網址放入下方（不需排序）
+ * 
+*'https://www.surveycake.com/s/lZO7r','https://www.surveycake.com/s/dprvB','https://www.surveycake.com/s/nZ49m','https://www.surveycake.com/s/VgnXP','https://www.surveycake.com/s/Z61zd','https://www.surveycake.com/s/MP2bD','https://www.surveycake.com/s/DPxLb','https://www.surveycake.com/s/X6BA0','https://www.surveycake.com/s/2ogk2'
+ * 
+ * 
  *
- * 並請注意：
- * 1. 除最後一項外，其餘項目用逗號分隔
- * 2. 網址請用引號包起來
  */
 
 
